4f0dcacd8b14fde7b4e2ee1b767d401e4dc52812
deven60 <bhattadevendra60@gmail.com>
2024-03-10 09:35:00 +0545

    react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
-
+let name = "Devendra Raj Bhatta"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <>
+      <nav>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </nav>
+      <div className="container">
+        <h1>Hello {name}</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur eligendi, id alias dolores tenetur at itaque temporibus adipisci qui cupiditate ea! Magni sit aperiam inventore similique est doloribus rem ad tenetur minima recusandae consectetur, reiciendis, eum sint cum autem amet temporibus. Expedita doloribus explicabo tempore?
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </div>
+    </>
   );
 }
+
+
 
 export default App;
